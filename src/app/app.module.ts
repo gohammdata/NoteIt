84f90de39fffc97c026c3b2b0c1d1bceba5cfed1
hammdata <gohammdata@gmail.com>
2020-03-05 17:14:26 -0500

    Added routing and api service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-import { LoginComponent } from './login/login.component';
 import { ListUserComponent } from './user/list-user/list-user.component';
+import { LoginComponent } from './login/login.component';
 import { AddUserComponent } from './user/add-user/add-user.component';
 import { EditUserComponent } from './user/edit-user/edit-user.component';
+import {routing} from "./app.routing";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ApiService} from "./service/api.service";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {TokenInterceptor} from "./core/interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    LoginComponent,
     ListUserComponent,
+    LoginComponent,
     AddUserComponent,
     EditUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    routing,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiService, {provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi : true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
